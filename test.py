72ada76d37a3393d5122f6a4f6d57d13eecaa00d
yushulx <lingxiao1002@gmail.com>
2022-08-08 15:01:39 +0800

    Update test code with mrz parser

--- test.py
+++ test.py
@@ -1,4 +1,47 @@
 import mrzscanner
+from mrz.checker.td1 import TD1CodeChecker
+from mrz.checker.td2 import TD2CodeChecker
+from mrz.checker.td3 import TD3CodeChecker
+from mrz.checker.mrva import MRVACodeChecker
+from mrz.checker.mrvb import MRVBCodeChecker
+
+def check(lines):
+    try:
+        td1_check = TD1CodeChecker(lines)
+        if bool(td1_check):
+            return "TD1", td1_check.fields()
+    except Exception as err:
+        pass
+    
+    try:
+        td2_check = TD2CodeChecker(lines)
+        if bool(td2_check):
+            return "TD2", td2_check.fields()
+    except Exception as err:
+        pass
+    
+    try:
+        td3_check = TD3CodeChecker(lines)
+        if bool(td3_check):
+            return "TD3", td3_check.fields()
+    except Exception as err:
+        pass
+    
+    try:
+        mrva_check = MRVACodeChecker(lines)
+        if bool(mrva_check):
+            return "MRVA", mrva_check.fields()
+    except Exception as err:
+        pass
+    
+    try:
+        mrvb_check = MRVBCodeChecker(lines)
+        if bool(mrvb_check):
+            return "MRVB", mrvb_check.fields()
+    except Exception as err:
+        pass
+    
+    return 'No valid MRZ information found'
 
 # set license
 mrzscanner.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==")
@@ -6,32 +49,34 @@ mrzscanner.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUi
 # initialize mrz scanner
 scanner = mrzscanner.createInstance()
 
+print('')
 # decodeFile()
+s = ""
 results = scanner.decodeFile("images/1.png")
 for result in results:
     print(result.text)
-    # print(result.confidence)
-    # print(result.x1)
-    # print(result.y1)
-    # print(result.x2)
-    # print(result.y2)
-    # print(result.x3)
-    # print(result.y3)
-    # print(result.x4)
-    # print(result.y4)
-
+    s += result.text + '\n'
+print('')
+print(check(s[:-1]))
 print('')
 
+s = ""
 results = scanner.decodeFile("images/2.png")
 for result in results:
     print(result.text)
-    
+    s += result.text + '\n'
+print('')
+print(check(s[:-1]))
 print('')
 
 # decodeMat()
+s = ""
 import cv2
 image = cv2.imread("images/3.jpg")
 results = scanner.decodeMat(image)
 for result in results:
     print(result.text)
+    s += result.text + '\n'
 
+print('')
+print(check(s[:-1]))
