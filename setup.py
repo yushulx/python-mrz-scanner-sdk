5afaea7a9b66bd1c27649834360af58a370daf90
yushulx <lingxiao1002@gmail.com>
2022-08-09 13:06:19 +0800

    To set up the model path dynamically, only source package is available for installation.

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ class CustomInstall(install):
 
 
 setup(name='mrz-scanner-sdk',
-      version='1.0.1',
+      version='1.0.2',
       description='Machine readable zone (MRZ) reading SDK for passport, Visa, ID card and travel document.',
       long_description=long_description,
       long_description_content_type="text/markdown",
